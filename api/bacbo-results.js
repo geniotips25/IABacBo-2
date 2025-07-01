96eacb0408d4de248c08cb51dc8719d966b68500
Wendel <geniotips2025@gmail.com>
2025-07-01 14:30:28 -0300

    Adicionando scraper real dos resultados BacBo via TipMiner

--- api/bacbo-results.js
+++ api/bacbo-results.js
@@ -1,40 +1,9 @@
+import { fetchBacBoResults } from '../lib/scraper.js';
+
 export async function GET() {
-  function rollDice() {
-    return Math.floor(Math.random() * 6) + 1;
-  }
+  const results = await fetchBacBoResults();
 
-  const playerDice1 = rollDice();
-  const playerDice2 = rollDice();
-  const playerTotal = playerDice1 + playerDice2;
-
-  const dealerDice1 = rollDice();
-  const dealerDice2 = rollDice();
-  const dealerTotal = dealerDice1 + dealerDice2;
-
-  let winner;
-  if (playerTotal > dealerTotal) {
-    winner = 'Player';
-  } else if (dealerTotal > playerTotal) {
-    winner = 'Dealer';
-  } else {
-    winner = 'Tie';
-  }
-
-  const result = {
-    playerDice: [playerDice1, playerDice2],
-    dealerDice: [dealerDice1, dealerDice2],
-    playerTotal,
-    dealerTotal,
-    winner,
-  };
-
-  await fetch('http://localhost:3000/api/patterns', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(result),
-  }).catch(() => {});
-
-  return new Response(JSON.stringify(result), {
+  return new Response(JSON.stringify(results), {
     headers: { 'Content-Type': 'application/json' },
   });
 }
